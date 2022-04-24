2cc42bede5e6d8e93ba8c2d1d3b92cb32e5749c8
Selva <selvadianett@gmail.com>
2022-04-24 10:33:06 -0400

    Sigo practicando commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    // Add smooth scrolling to all links
+    // Agregar smooth scrolling a todos los links
     $("a")("aside").on('click', function(event) {
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
-        // Prevent default anchor click behavior
+        // Evitar el comportamiento de clic de anclaje 
         event.preventDefault();
         // Store hash
         var hash = this.hash;
